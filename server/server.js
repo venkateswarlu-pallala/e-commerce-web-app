53617a63c0142c49841342f3601371b1cc6fe2e1
venkateswarlu-pallala <venki44456@gmailcom.com>
2025-08-30 14:09:24 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -6,13 +6,29 @@ const connectDB = require('./config/db'); // Assuming you have a DB connection f
 const userRoutes = require('./routes/authroutes'); // Assuming authroutes.js
 const orderRoutes = require('./routes/orderRoutes'); // Assuming orderRoutes.js
 const shopRoutes = require('./routes/shopRoutes'); // <--- THIS IS THE KEY IMPORT
-
+const cors = require('cors');
 dotenv.config();
 connectDB(); // Connect to MongoDB
 
 const app = express();
 
-app.use(express.json()); // Body parser for JSON data
+const allowedOrigins = [
+  'http://localhost:3000',                 // Local React dev
+  'https://v-shop-website.netlify.app'          // Your Netlify deployed site
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+  credentials: true, // Allow cookies to be sent
+}));
 
 // API Routes
 app.use('/api/users', userRoutes); // For /api/users/register and /api/users/login
