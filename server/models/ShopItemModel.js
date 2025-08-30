7de8c021e5e7f154936823a042033c1c6aea22f4
venkateswarlu-pallala <venki44456@gmailcom.com>
2025-08-30 15:20:53 +0530

    update all

--- server/models/ShopItemModel.js
+++ server/models/ShopItemModel.js
@@ -1,47 +1,11 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    orderItems: [
-      {
-        name: { type: String, required: true },
-        quantity: { type: Number, required: true },
-        price: { type: Number, required: true },
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true,
-          ref: 'ShopItem', // ensure your ShopItem model is exported as 'ShopItem'
-        },
-      },
-    ],
-    shippingAddress: {
-      address: { type: String, required: true },
-      city: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-      default: 0.0,
-    },
-    isPaid: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    paidAt: {
-      type: Date,
-    },
-  },
-  {
-    timestamps: true, // auto adds createdAt & updatedAt
-  }
-);
+const shopItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  imageUrl: { type: String },
+});
 
-module.exports = mongoose.models.Order || mongoose.model('Order', orderSchema);
+module.exports = mongoose.model('shopItem', shopItemSchema);
